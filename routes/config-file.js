1b9330590c21fdf6c7969e5ff3fdc84a95a2680f
Andres Ruiz <andrsruiz@gmail.com>
2020-11-03 13:55:28 -0600

    Create Router for config file

---
+++ routes/config-file.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+/* Config File Controller */
+const CONFIG = require(path.join(__dirname, '../controllers/config-file.js'));
+
+/* Config File Routes */
+router.post('/', CONFIG.CreateConfigFile);
+
+module.exports = router;
