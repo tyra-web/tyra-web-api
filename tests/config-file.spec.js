681e19f2522f807bb6bdd2eaa302c3f676023be1
Andres Ruiz <andrsruiz@gmail.com>
2020-11-04 12:45:11 -0600

    Test for auth errors

--- tests/config-file.spec.js
+++ tests/config-file.spec.js
@@ -2,7 +2,11 @@ const request = require('supertest');
 const server = require('../mock_server');
 
 describe('Configuration File', () => {
-	it('Should create an initial configuration file', () => {
-		return request(server).post('/config').send({}).expect(201);
+	it('Should get auth error without token when updating', () => {
+		return request(server).post('/config').send({}).expect(401);
+	});
+
+	it('Should get auth error without token when getting file', () => {
+		return request(server).get('/config/setup').expect(401);
 	});
 });
