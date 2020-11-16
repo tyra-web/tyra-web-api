d33a19a6da1fd3b17a1554f27fde45165a7ce641
Andres Ruiz <andrsruiz@gmail.com>
2020-11-16 14:07:30 -0600

    Change Routes

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 require('dotenv').config();
 
-const EXPRESS = require('express');
-const CORS = require('cors');
-const BODY_PARSER = require('body-parser');
-const MORGAN = require('morgan');
-const SERVE_STATIC = require('serve-static');
-const FILE_UPLOAD = require('express-fileupload');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const serveStatic = require('serve-static');
+const fileUpload = require('express-fileupload');
 
 /* Defining port */
 const PORT = process.env.PORT || 3000;
 
 /* Mongo DB Connection */
-const MONGOOSE = require('mongoose');
-MONGOOSE.connect(process.env.MONGODB_TYRAWEB_TEST, { useNewUrlParser: true });
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_TYRAWEB_TEST, { useNewUrlParser: true });
 
-const DB = MONGOOSE.connection;
-DB.on('error', (error) => console.error(error));
-DB.once('open', () => console.log('Connected to database!'));
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to database!'));
 
 /* Defining routes */
 const CONFIG_ROUTER = require('./routes/config-file');
@@ -28,35 +28,35 @@ const SERVICE_ROUTER = require('./routes/service');
 const DAY_SCHEDULE_ROUTER = require('./routes/day_schedule');
 
 /* Defining app */
-const APP = EXPRESS();
+const app = express();
 
 /* Defining middlewares */
-APP.use(MORGAN('combined'));
-APP.use(BODY_PARSER.json());
-APP.use(BODY_PARSER.urlencoded({ extend: true }));
-APP.use(CORS());
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extend: true }));
+app.use(cors());
 
 /* Static Folder */
-APP.use(SERVE_STATIC(__dirname + '../client/dist'));
-APP.use(EXPRESS.static('uploads', { etag: false }));
+app.use(serveStatic(__dirname + '../client/dist'));
+app.use(express.static('uploads', { etag: false }));
 
 /* Upload Files */
-APP.use(FILE_UPLOAD({ createParentPath: true }));
+app.use(fileUpload({ createParentPath: true }));
 
 /* Using routes */
-APP.use(process.env.TYRAWEB_ROUTE_CONFIG, CONFIG_ROUTER);
-APP.use(process.env.TYRAWEB_ROUTE_USERS, USER_ROUTER);
-APP.use(process.env.TYRAWEB_ROUTE_BREED, BREED_ROUTER);
-APP.use(process.env.TYRAWEB_ROUTE_CLIENTS, CLIENT_ROUTER);
-APP.use(process.env.TYRAWEB_ROUTE_PETS, PET_ROUTER);
-APP.use(process.env.TYRAWEB_ROUTE_SERVICES, SERVICE_ROUTER);
-APP.use(process.env.TYRAWEB_ROUTE_DAY_SCHEDULES, DAY_SCHEDULE_ROUTER);
+app.use('/api' + process.env.TYRAWEB_ROUTE_CONFIG, CONFIG_ROUTER);
+app.use('/api' + process.env.TYRAWEB_ROUTE_USERS, USER_ROUTER);
+app.use('/api' + process.env.TYRAWEB_ROUTE_BREED, BREED_ROUTER);
+app.use('/api' + process.env.TYRAWEB_ROUTE_CLIENTS, CLIENT_ROUTER);
+app.use('/api' + process.env.TYRAWEB_ROUTE_PETS, PET_ROUTER);
+app.use('/api' + process.env.TYRAWEB_ROUTE_SERVICES, SERVICE_ROUTER);
+app.use('/api' + process.env.TYRAWEB_ROUTE_DAY_SCHEDULES, DAY_SCHEDULE_ROUTER);
 
-APP.listen(PORT, error => {
+app.listen(PORT, error => {
 	if(error)
 		return console.error(error);
 
 	return console.log('Server is listening on ' + PORT);
 })
 
-module.exports = APP;
+module.exports = app;
